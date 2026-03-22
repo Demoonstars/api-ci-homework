fdb8f51502f9db64b0dea3193281e3be8eb668d6
Anton <Antonpopikhin@gmail.com>
2026-03-22 16:52:52 +0300

    Add MobileBankApiTest

---
+++ src/test/java/ru/netology/rest/MobileBankApiTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+class MobileBankApiTest {
+    @Test
+    void shouldReturnDataAccordingToSchema() {
+      // Предусловия
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/accounts")
+      // Проверки
+      .then()
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+}
